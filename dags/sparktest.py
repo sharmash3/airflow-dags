83a7504858ee14265e1b32dbaeaf446b0d410d27
SHARMA SURAJ <ssharmaparfr@gmail.com>
2025-07-07 01:47:17 +0200

    Update sparktest.py

--- dags/sparktest.py
+++ dags/sparktest.py
@@ -28,6 +28,7 @@ with DAG(
         "spark.kubernetes.authenticate.driver.serviceAccountName": "spark",
         "spark.kubernetes.driver.pod.name": "spark-driver-transaction-filter",
         "spark.kubernetes.container.image.pullPolicy": "IfNotPresent"
-    }
+    },
+        verbose=True,
 )
 
