3dd4d70a7c577e4c3afa68fe8cf27bbb4db13738
SHARMA SURAJ <ssharmaparfr@gmail.com>
2025-07-07 01:28:13 +0200

    Update sparktest.py

--- dags/sparktest.py
+++ dags/sparktest.py
@@ -13,20 +13,21 @@ with DAG(
     catchup=False
 ) as dag:
 
-    spark_job = SparkSubmitOperator(
-        task_id='transaction_filter',
-        application='/opt/spark-apps/transaction-filter.jar',
-        java_class='com.example.TransactionFilter',
-        application_args=['/opt/spark-apps/transactions.csv'],
-        conn_id='spark_default',  # Make sure this connection is set up properly
-        verbose=True,
-        conf={
-            "spark.master": "k8s://https://kubernetes.default.svc",  # Adjust if needed
-            "spark.submit.deployMode": "cluster",
-            "spark.kubernetes.container.image": "apache/spark:3.5.3",
-            "spark.kubernetes.namespace": "default",
-            "spark.kubernetes.authenticate.driver.serviceAccountName": "spark",
-            "spark.kubernetes.driver.pod.name": "spark-driver-transaction-filter",
-            "spark.kubernetes.container.image.pullPolicy": "IfNotPresent"
-        }
-    )
+   spark_job = SparkSubmitOperator(
+    task_id='transaction_filter',
+    application='/opt/spark-apps/transaction-filter.jar',
+    main_class='com.example.TransactionFilter',  # <-- fixed here
+    application_args=['/opt/spark-apps/transactions.csv'],
+    conn_id='spark_default',
+    verbose=True,
+    conf={
+        "spark.master": "k8s://https://kubernetes.default.svc",
+        "spark.submit.deployMode": "cluster",
+        "spark.kubernetes.container.image": "apache/spark:3.5.3",
+        "spark.kubernetes.namespace": "default",
+        "spark.kubernetes.authenticate.driver.serviceAccountName": "spark",
+        "spark.kubernetes.driver.pod.name": "spark-driver-transaction-filter",
+        "spark.kubernetes.container.image.pullPolicy": "IfNotPresent"
+    }
+)
+
