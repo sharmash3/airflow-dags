28adee3cb92614996bba5196310fd61b3c31c87e
SHARMA SURAJ <ssharmaparfr@gmail.com>
2025-07-06 19:42:17 +0200

    Update sparktest.py

--- dags/sparktest.py
+++ dags/sparktest.py
@@ -6,17 +6,19 @@ default_args = {
     'start_date': datetime(2025, 7, 6),
 }
 
-with DAG('run_transaction_filter',
-         schedule_interval=None,
-         default_args=default_args,
-         catchup=False) as dag:
+with DAG(
+    dag_id='run_transaction_filter',
+    schedule=None,  # Changed from schedule_interval=None
+    default_args=default_args,
+    catchup=False
+) as dag:
 
     spark_job = SparkSubmitOperator(
         task_id='transaction_filter',
         application='/opt/spark-apps/transaction-filter.jar',
         java_class='com.example.TransactionFilter',
         application_args=['/opt/spark-apps/transactions.csv'],
-        conn_id='spark_default',  # make sure this connection exists in Airflow
+        conn_id='spark_default',  # Ensure this connection is defined in Airflow
         executor_memory='1g',
         driver_memory='1g',
         verbose=True,
