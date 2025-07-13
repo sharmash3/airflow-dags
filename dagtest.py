09b855ce309f996431c30ed1b5ce36b8d80fca57
SHARMA SURAJ <ssharmaparfr@gmail.com>
2025-07-13 12:34:27 +0200

    Update dagtest.py

--- dagtest.py
+++ dagtest.py
@@ -1,32 +1,59 @@
 from airflow import DAG
-from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
 from datetime import datetime
 
-default_args = {
-    'start_date': datetime(2025, 7, 6),
-}
-
 with DAG(
-    dag_id='run_transaction_filter',
-    schedule=None,
-    default_args=default_args,
-    catchup=False
+    dag_id="transaction_filter_spark_k8s",
+    start_date=datetime(2025, 7, 13),
+    schedule_interval=None,
+    catchup=False,
+    tags=["spark", "kubernetes", "transaction"],
 ) as dag:
 
-   spark_job = SparkSubmitOperator(
-    task_id='transaction_filter',
-    application='opt/spark/jars/transaction-filter-1.0.jar',
-    java_class='com.example.TransactionFilter',  # <-- fixed here
-    application_args=['/opt/spark/data/transactions.csv'],
-    conn_id='spark_default',
-    verbose=True,
-    conf={
-        "spark.master": "k8s://https://kubernetes.default.svc",
-        "spark.submit.deployMode": "cluster",
-        "spark.kubernetes.container.image": "transaction-filter:2.0",
-        "spark.kubernetes.namespace": "default",
-        "spark.kubernetes.authenticate.driver.serviceAccountName": "spark",
-        "spark.kubernetes.driver.pod.name": "spark-driver-transaction-filter",
-        "spark.kubernetes.container.image.pullPolicy": "IfNotPresent"
-    }
-)
+    transaction_filter = SparkKubernetesOperator(
+        task_id="submit_transaction_filter",
+        namespace="default",
+        application_manifest={
+            "apiVersion": "sparkoperator.k8s.io/v1beta2",
+            "kind": "SparkApplication",
+            "metadata": {
+                "name": "transaction-filter",
+                "namespace": "default",
+            },
+            "spec": {
+                "type": "Scala",
+                "mode": "cluster",
+                "image": "transaction-filter:2.0",
+                "imagePullPolicy": "IfNotPresent",
+                "mainClass": "com.example.TransactionFilter",
+                "mainApplicationFile": "local:///opt/spark/jars/transaction-filter-1.0.jar",
+                "arguments": [
+                    "/opt/spark/data/transactions.csv"
+                ],
+                "sparkVersion": "3.5.1",
+                "restartPolicy": {
+                    "type": "Never"
+                },
+                "driver": {
+                    "cores": 1,
+                    "coreLimit": "1200m",
+                    "memory": "512m",
+                    "labels": {
+                        "version": "3.5.1"
+                    },
+                    "serviceAccount": "spark"
+                },
+                "executor": {
+                    "cores": 1,
+                    "instances": 1,
+                    "memory": "512m",
+                    "labels": {
+                        "version": "3.5.1"
+                    }
+                }
+            }
+        },
+        do_xcom_push=False
+    )
+
+    transaction_filter
